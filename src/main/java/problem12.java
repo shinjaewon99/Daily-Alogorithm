3d77ce20567a608f9d0aad6254dd81422f530013
shinjaewon99 <tlswodnjs99@naver.com>
2023-01-20 11:49:32 +0900

    feat : problem12

---
+++ src/main/java/problem12.java
@@ -0,0 +1,71 @@
+
+
+/*
+두 개 뽑아서 더하기
+문제 설명
+정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+numbers의 길이는 2 이상 100 이하입니다.
+numbers의 모든 수는 0 이상 100 이하입니다.
+입출력 예
+numbers	result
+[2,1,3,4,1]	[2,3,4,5,6,7]
+[5,0,2,7]	[2,5,7,9,12]
+입출력 예 설명
+입출력 예 #1
+
+2 = 1 + 1 입니다. (1이 numbers에 두 개 있습니다.)
+3 = 2 + 1 입니다.
+4 = 1 + 3 입니다.
+5 = 1 + 4 = 2 + 3 입니다.
+6 = 2 + 4 입니다.
+7 = 3 + 4 입니다.
+따라서 [2,3,4,5,6,7] 을 return 해야 합니다.
+입출력 예 #2
+
+2 = 0 + 2 입니다.
+5 = 5 + 0 입니다.
+7 = 0 + 7 = 5 + 2 입니다.
+9 = 2 + 7 입니다.
+12 = 5 + 7 입니다.
+따라서 [2,5,7,9,12] 를 return 해야 합니다.
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            // i+1 인덱스 부터 시작함으로써 i가 증가할때
+            // i보다 작은 index에서 부터 시작 할 필요가 없음으로,
+            // i+1 인덱스 부터 조건을 건다.
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+
+                //ArrayList의 indexOf 메소드의 기능은
+                // 넘어온 인자값과 동일한 데이터가 있다면 그 인덱스 값을 반환해준다,
+                // 없으면 -1를 리턴합니다.
+                // 따라서, 위 코드는 중복된 값이 없애기 위해 구현
+
+                if (result.indexOf(sum) < 0) {
+                    result.add(sum);
+                }
+
+            }
+        }
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = result.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
