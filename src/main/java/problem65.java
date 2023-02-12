1dffb4bb133af4bf231c4193ca3012f9c5017883
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-12 16:26:54 +0900

    feat : problem65

---
+++ src/main/java/problem65.java
@@ -0,0 +1,46 @@
+/*
+문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+예제 입력 1
+5
+5
+2
+3
+4
+1
+예제 출력 1
+1
+2
+3
+4
+5
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class problem65 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // 간단한 정렬 문제다.
+        int size = in.nextInt();
+        int [] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = in.nextInt();
+        }
+        // 오름차순으로 정렬
+        Arrays.sort(array);
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
