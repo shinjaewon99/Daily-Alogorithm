580bd9de6b6236c3a22a889eb03c3d68308466ec
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-18 17:43:35 +0900

    feat : problem97

---
+++ src/main/java/problem97.java
@@ -0,0 +1,64 @@
+/*
+문제
+2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+
+출력
+첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+
+예제 입력 1
+5
+3 4
+1 1
+1 -1
+2 2
+3 3
+예제 출력 1
+1 -1
+1 1
+2 2
+3 3
+3 4
+*/
+
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class problem97 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+
+        int n = in.nextInt();
+        int [][] arr = new int [n][2];
+
+        for(int i = 0; i < n; i++) {
+            arr[i][0] = in.nextInt();
+            arr[i][1] = in.nextInt();
+        }
+
+        // 2차원 배열 오름차순 정렬
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
