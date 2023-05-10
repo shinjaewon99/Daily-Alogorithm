bfac0b0924bf1118ec8c5d5e22da3507091ed7af
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-10 11:12:38 +0900

    feat : problem389

---
+++ src/main/java/problem389.java
@@ -0,0 +1,37 @@
+/*
+0 떼기
+문제 설명
+정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+2 ≤ n_str ≤ 10
+n_str이 "0"으로만 이루어진 경우는 없습니다.
+입출력 예
+n_str	result
+"0010"	"10"
+"854020"	"854020"
+입출력 예 설명
+입출력 예 #1
+
+"0010"의 가장 왼쪽에 연속으로 등장하는 "0"을 모두 제거하면 "10"이 됩니다.
+입출력 예 #2
+
+"854020"는 가장 왼쪽에 0이 없으므로 "854020"을 return합니다.
+*/
+public class problem389 {
+    public class Solution {
+        public String solution(String n_str) {
+            int idx = 0;
+            // 0을 만날때까지
+            while (n_str.charAt(idx) == '0') {
+                idx++;
+            }
+            // 0으로만 이루어진 문자열일 경우
+            if (idx == n_str.length()) {
+                return "0";
+            }
+            return n_str.substring(idx);
+        }
+    }
+
+}
