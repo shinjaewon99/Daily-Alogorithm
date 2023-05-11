560e1a832e4808cde2564b0ea912a31a70b82a4e
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-11 21:02:17 +0900

    feat : problem396

---
+++ src/main/java/problem396.java
@@ -0,0 +1,51 @@
+/*
+수열과 구간 쿼리 1
+문제 설명
+정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [s, e] 꼴입니다.
+
+각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 arr[i]에 1을 더합니다.
+
+위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+1 ≤ arr의 길이 ≤ 1,000
+0 ≤ arr의 원소 ≤ 1,000,000
+1 ≤ queries의 길이 ≤ 1,000
+0 ≤ s ≤ e < arr의 길이
+입출력 예
+arr	queries	result
+[0, 1, 2, 3, 4]	[[0, 1],[1, 2],[2, 3]]	[1, 3, 4, 4, 4]
+입출력 예 설명
+입출력 예 #1
+
+각 쿼리에 따라 arr가 다음과 같이 변합니다.
+i	queries[i]	arr
+-	-	[0, 1, 2, 3, 4]
+0	[0,1]	[1, 2, 2, 3, 4]
+1	[1,2]	[1, 3, 3, 3, 4]
+2	[2,3]	[1, 3, 4, 4, 4]
+따라서 [1, 3, 4, 4, 4]를 return 합니다.
+*/
+public class problem396 {
+    class Solution {
+        public int[] solution(int[] arr, int[][] queries) {
+            int[] answer = new int[arr.length];
+
+            for (int i = 0; i < queries.length; i++) {
+                int start = queries[i][0];
+                int end = queries[i][1];
+
+                // start 인덱스부터, end 인덱스 까지 arr 배열 값 증가
+                for (int j = start; j <= end; j++) {
+                    arr[j] += 1;
+                }
+            }
+
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = arr[i];
+            }
+
+            return answer;
+        }
+    }
+}
