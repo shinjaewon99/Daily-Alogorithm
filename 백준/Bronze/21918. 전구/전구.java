138008e743b0cc059c27051063dbb4c56bc71c8a
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2023-07-11 13:36:34 +0900

    [Bronze II] Title: 전구, Time: 736 ms, Memory: 44964 KB -BaekjoonHub

--- 백준/Bronze/21918. 전구/전구.java
+++ 백준/Bronze/21918. 전구/전구.java
@@ -4,36 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt(); // 전구의 개수
-        int m = in.nextInt(); // 명령어의 개수
+        int n = in.nextInt(); // 전구의 갯수
+        int m = in.nextInt(); // 명령어의 갯수
 
         int[] light = new int[n]; // 전구의 상태 배열
         for (int i = 0; i < n; i++) {
             light[i] = in.nextInt();
         }
 
-        for (int i = 0; i < m; i++) {
+        while (m-- > 0) {
             int a = in.nextInt(); // 명령어 번호
             int b = in.nextInt();
             int c = in.nextInt();
 
+            // 1번 명령어
             if (a == 1) {
                 light[b - 1] = c;
             } else if (a == 2) {
-                for (int j = b - 1; j < c; j++) {
-                    if (light[j] == 0) {
-                        light[j] = 1;
-                    } else {
-                        light[j] = 0;
+                for (int i = b - 1; i < c; i++) {
+                    // 전구가 꺼져 있는경우
+                    if (light[i] == 0) {
+                        light[i] = 1;
+                    }
+                    // 전구가 켜져 있는 경우
+                    else{
+                        light[i] = 0;
                     }
                 }
             } else if (a == 3) {
-                for (int j = b - 1; j < c; j++) {
-                    light[j] = 0;
+                for (int i = b - 1; i < c; i++) {
+                    light[i] = 0;
                 }
-            } else if (a == 4) {
-                for (int j = b - 1; j < c; j++) {
-                    light[j] = 1;
+
+            } else if(a == 4) {
+                for (int i = b - 1; i < c; i++) {
+                    light[i] = 1;
                 }
             }
         }
@@ -41,5 +46,6 @@ public class Main {
         for (int i = 0; i < n; i++) {
             System.out.print(light[i] + " ");
         }
+
     }
 }
