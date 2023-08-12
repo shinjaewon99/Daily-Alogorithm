4cd4e3f2da957af79f55498641815387e43ec70d
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2023-08-12 21:48:34 +0900

    Update 피보나치 수 2.java

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-    
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
 
-        int size = in.nextInt();
-        long[] arr = new long[91];
+        // 1. 정수 n 입력
+        int number = in.nextInt();
 
-        arr[0] = 0;
-        arr[1] = 1;
-        for (int i = 2; i <= size; i++) {
-            arr[i] += arr[i - 2] + arr[i - 1];
+        System.out.println(fibo(number));
+    }
 
+    private static long fibo(int number) {
+        // 2. 요구사항의 배열 크기 지정
+        long[] dp = new long[91];
+
+        // 3. dp 배열의 값 할당
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 2; i <= number; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        System.out.print(arr[size]);
-
+        return dp[number];
     }
 }
-
-
