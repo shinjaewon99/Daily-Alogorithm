7607f48969920643752424056fe364920a3c8cae
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2023-08-17 21:40:33 +0900

    Update 1로 만들기.java

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,24 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int num = in.nextInt();
+        // 1. 정수 N 입력
+        int number = in.nextInt();
+        int[] dp = new int[number + 1];
 
-        // dp 문제에서는 배열을 한개 더 크게만든다.
-        int[] dp = new int[num + 1];
-
-
-        // dp에서는 초기값 초기화가 중요
+        // 2. dp 배열 값 초기화 
+        dp[0] = 0;
         dp[1] = 0;
 
-
-        // 입력값이 3일 경우, 1을 빼면 연산횟수 2회 3을 나누면 1회
-        for (int i = 2; i <= num; i++) {
+        // dp[3] = 1
+        // dp[4] = 2
+        // dp[5] = 3
+        //.. dp[9] = 2;
+        for (int i = 2; i <= number; i++) {
             dp[i] = dp[i - 1] + 1;
 
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1); 
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-        }
+            // 2로 나눠질 경우
+            if(i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
 
-        System.out.println(dp[num]);
+            // 3으로 나눠질 경우
+            if(i % 3 == 0){
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+        System.out.println(dp[number]);
     }
 }
