5be2c01284862d2ee355dc1604fe333efe4bfcd7
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2023-08-18 23:23:08 +0900

    Update 1， 2， 3 더하기.java

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
 
-        int size = in.nextInt();
+        // 1. 테스트 갯수 입력
+        int testSize = in.nextInt();
+        int[] numbers = new int[testSize];
+        int[] dp = new int[12];
 
-        int [] arr = new int [11];
-        // 초기값
-        // 1, 2, 3으로 값을 만드는 것임으로
-        arr[1] = 1; // 1
-        arr[2] = 2; // 1+1 , 2
-        arr[3] = 4; // 1+1+1, 1+2, 2+1
+        dp[1] = 1; // 1
+        dp[2] = 2; // 11, 2
+        dp[3] = 4; // 111, 12, 21, 3
 
-        for(int i = 0; i < size; i++) {
-            int temp = in.nextInt();
+        // dp[4] = (1+1+1+1), (1+1+2), (1+2+1), (1+3), (2+1+1), (2+2), (3+1)
+        // dp[4] = dp[3] + dp[2] + dp[1]
+        // 1로 시작하는 식은 dp[3] 
+        // 2로 시작하는 식은 dp[2] 
+        // 3으로 시작하는 식은 dp[1]
 
-            for (int j = 4; j <= temp; j++) {
-                arr[j] = arr[j - 3] + arr[j - 2] + arr[j - 1];
+        // 2. 테스트 갯수만큼 정수 입력
+        for (int i = 0; i < testSize; i++) {
+            numbers[i] = in.nextInt();
+
+            for (int j = 4; j <= numbers[i]; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
             }
-            System.out.println(arr[temp]);
-
+            System.out.println(dp[numbers[i]]);
         }
     }
 }
-
