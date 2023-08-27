8e419a362490b981f4947861f73292b37877520a
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2023-08-27 21:07:19 +0900

    [Silver III] Title: 파일 정리, Time: 1928 ms, Memory: 89296 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -3,23 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int size = in.nextInt(); // 파일의 갯수
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < size; i++) {
-            String[] file = in.next().split("\\."); // "."을 기준으로 split을 한다
-            map.put(file[1], map.getOrDefault(file[1], 0) + 1); // Map 객체에 file[1] 문자열이 없을 경우 0, 있을경우 누적합
+
+        // 1. 파일의 갯수 입력
+        int fileSize = in.nextInt();
+
+        // 2. 파일의 갯수만큼 파일 입력
+        String[] files = new String[fileSize];
+        for (int i = 0; i < fileSize; i++) {
+            files[i] = in.next();
+        }
+
+        // 3. 파일의 확장자 저장
+        Map<String, Integer> store = new HashMap<>();
+
+        for (int i = 0; i < fileSize; i++) {
+            String file = files[i];
+            String[] commaSplit = file.split("\\.");
+            store.put(commaSplit[1], store.getOrDefault(commaSplit[1], 0) + 1);
 
         }
 
-        List<String> list = new ArrayList<>(map.keySet()); // List 객체에 Map의 key값을 담아준다.
-        Collections.sort(list);
+        // 4. 확장자의 이름순으로 출력
+        List<String> comparator = new ArrayList<>(store.keySet());
 
+        // 5. Map 객체에서 List 객체로 담은후, 내림차순 정렬
+        Collections.sort(comparator);
 
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i) + " " + map.get(list.get(i))); // list 반환, map의 key값에 해당하는 value값 반환
-            System.out.println();
+        for (int i = 0; i < comparator.size(); i++) {
+            System.out.println(comparator.get(i) + " " + store.get(comparator.get(i)));
         }
-
-
     }
 }
