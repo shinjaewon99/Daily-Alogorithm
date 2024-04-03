7b79d534abcd0f1cff211d917bf38e1153a3542b
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-04-03 21:31:42 +0900

    [level 2] Title: 주식가격, Time: 10.93 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,11 +2,15 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        // 1. 2중 for문을 통해 가격이 떨어지지 않는 기간 탐색
-        for(int i = 0; i < prices.length; i++){
+        // 1. prices 배열의 값 비교
+        for(int i = 0; i< prices.length; i++){
             for(int j = i + 1; j < prices.length; j++){
                 answer[i]++;
-                if(prices[i] > prices[j]) break;
+                
+            // 2. 가격이 떨어졌는지 검증
+              if(prices[i] > prices[j]){
+                  break;
+              }
             }
         }
         
