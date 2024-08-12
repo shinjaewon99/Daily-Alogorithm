de043ca7a2b2cc2472377b63264124e036e24729
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-08-12 17:51:38 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,21 +1,22 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
-        int cards1Lenght = 0;
+        int cards1Length = 0;
         int cards2Length = 0;
         
-        for(int i = 0; i < goal.length; i++) {
+        for(int i = 0; i < goal.length; i++){
+            
+            // 비교할 문자열
             String target = goal[i];
             
-            // cards1.length의 크기와 비교하는 이유, 인덱스를 초과하게되면 접근하는것 자체로도 예외 발생
-            if(cards1Lenght < cards1.length && target.equals(cards1[cards1Lenght])){
-                cards1Lenght++;
-            }else if(cards2Length < cards2.length && target.equals(cards2[cards2Length])) {
+            // 카드를 한칸씩 앞으로 땡기면서 비교
+            if(cards1Length < cards1.length && target.equals(cards1[cards1Length])){
+                cards1Length++;
+            }else if(cards2Length < cards2.length && target.equals(cards2[cards2Length])){
                 cards2Length++;
-            }else{
-                return "No";
+            }else {
+                answer = "No";
             }
-            
         }
         
         return answer;
