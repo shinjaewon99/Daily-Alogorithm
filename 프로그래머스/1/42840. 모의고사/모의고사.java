e6fabf3d5475006ef67b96edb679e3a09cf03155
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-08-21 10:02:18 +0900

    [level 1] Title: 모의고사, Time: 3.17 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,41 +1,44 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int[] solution(int[] answers) {
+        int[] method1 = {1,2,3,4,5};
+        int[] method2 = {2,1,2,3,2,4,2,5};
+        int[] method3 = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] count = new int[3];
+        
+        // 찍는 방식과 답을 비교
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == method1[i % 5]){
+                count[0]++;
+            }
+            if(answers[i] == method2[i % 8]){
+                count[1]++;
+            }
+            if(answers[i] == method3[i % 10]){
+                count[2]++;
+            }
+        }
+
+        // 최대로 맞춘 문제 갯수를 구한다 
+        int maxCount = Math.max(count[0],Math.max(count[1], count[2]));
+        
         List<Integer> store = new ArrayList<>();
-        int[] answer = new int[3];
-        int[] first = {1, 2, 3, 4, 5};
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-        // 1. 찍는 방법에 따라 맞힌 갯수를 세어 준다.
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == first[i % 5]) {
-                answer[0]++;
-            }
-            if (answers[i] == second[i % 8]) {
-                answer[1]++;
-            }
-            if (answers[i] == third[i % 10]) {
-                answer[2]++;
-            }
+        
+        if(maxCount == count[0]){
+            store.add(1);
         }
-
-        // 2. 문제를 가장 많이 맞힌 갯수를 저장한다.
-        int max = Arrays
-                .stream(answer)
-                .max()
-                .orElse(0);
-
-        for (int i = 0; i < answer.length; i++) {
-            if(answer[i] == max){
-                store.add(i + 1);
-            }
+        if(maxCount == count[1]){
+            store.add(2);
         }
-
-        // 3. store 객체를 정수형 배열로 변환 한다.
-        return store.stream().mapToInt(Integer::intValue).toArray();
+        if(maxCount == count[2]){
+            store.add(3);
+        }
+        
+        
+        
+        return store.stream().mapToInt(i -> i).toArray();
     }
 }
