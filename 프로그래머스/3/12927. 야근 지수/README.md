2eec8524335e4c61df8eef154f304166421b7bb4
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-08-30 10:51:52 +0900

    [level 3] Title: 야근 지수, Time: 136.06 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
 
 ### 성능 요약
 
-메모리: 68.1 MB, 시간: 119.52 ms
+메모리: 67.6 MB, 시간: 136.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 16:31:13
+2024년 08월 30일 10:51:51
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,4 +1,5 @@
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Collections;
 
@@ -6,40 +7,27 @@ class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
         
-        /**
-        * 우선순위 큐를 사용해 최대, 최소를 빠르게 얻음
-        *   [4, 3, 3]의 경우 야근 피로도는 4^2 + 3^2 + 3^2 = 34
-            [3, 2, 2]의 경우 야근 피로도는 3^2 + 2^2 + 2^2 = 17
-            [2, 2, 2]의 경우 야근 피로도는 2^2 + 2^2 + 2^2 = 12
-        **/
+        // 우선순위 큐는 일반적으로 오름차순이므로, 내림차순으로 해준다.
         PriorityQueue<Integer> store = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int work : works) {
+        
+        for(int work : works){
             store.add(work);
         }
         
-        for(int i = 0; i < n; i++) {
-            int number = store.poll();
-            
-            if(number == 0) return 0;
-            
-            store.add(number - 1);
+        // 퇴근 까지 남은시간 동안 반복
+        while(n > 0){
+            int working = store.poll();
+            if(working == 0) break;
+            working--;
+            store.add(working);
+            n--;
         }
         
-        while(!store.isEmpty()){
-            answer += Math.pow(store.poll(), 2);
+        for(int work : store){
+            answer += work * work;
         }
         
-        /**
-        for문으로 하게 되면, store 객체의 size가 poll() 메소드로 인해 가변적으로 변해서
-        결과값이 달라짐
-        
-        for(int i = 0; i < store.size(); i++) {
-             answer += Math.pow(store.poll(), 2);
-        }
-        **/
         
         return answer;
     }
 }
-
