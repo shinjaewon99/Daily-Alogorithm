0d18a77ce2aed64ca1ac6cdd4e5ac6451a04344e
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-09-06 13:31:17 +0900

    [Silver V] Title: 셀프 넘버, Time: 116 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,31 +1,30 @@
 public class Main {
-public static void main(String[] args) {
-		boolean[] noneSelfNumber = new boolean[10001];
+    public static void main(String[] args) {
+        boolean[] store = new boolean[10001];
 
-		for (int i = 1; i <= 10000; i++) {
-			int check = result(i);
-			//10000이 넘는 숫자는 필요 X
-			if(check < 10001){
-				noneSelfNumber[check] = true;
-			}
-		}
-    for (int i = 1; i <= 10000; i++) {   
-                if (noneSelfNumber[i]==false) {
-                    System.out.println(i);
-                }
+        for(int i = 1; i < 10001; i++){
+            int num = selfNum(i);
+
+            if(num <= 10000) {
+                store[num] = true;
             }
-}
+        }
 
-public static int result(int number){
-	int sum = number;
-	while (number !=0){
-		sum += (number% 10);
-		number = number / 10;
-	}
-	return sum;
-}
-}
-    	
+         for (int i = 1; i <= 10000; i++) {
+            if (!store[i]) {
+                System.out.println(i);
+            }
+        }
+    }
 
+    private static int selfNum(int number) {
+        int sum = number;
 
-	
+        while(number != 0){
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
