0f6dbb83b3882ecfee81adbc0b28264f8aa281fd
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-09-07 12:54:17 +0900

    [Silver V] Title: 단어 정렬, Time: 764 ms, Memory: 59912 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,58 +1,30 @@
+import java.util.*;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
-
-public class Main {
-
+class Main {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
 
-
-
-        int size = in.nextInt();
-
-
-        String [] arr = new String[size];
-
-        /**
-         * 개행 버림 (Scanner로 입력받을시 nextInt()로 정수를 받은뒤 nextLine()을 쓰면 개행에 저장됨,
-         * (개행을 한번 버려야 정상적으로 작동)
-         */
+        int number = in.nextInt();
         in.nextLine();
-
-        for(int i = 0; i<size; i++) {
-            arr[i] = in.nextLine();
+        
+        Set<String> words = new HashSet<>();
+        for(int i = 0; i < number; i++){
+            words.add(in.nextLine());
         }
 
-        Arrays.sort(arr,new Comparator<String>() {
-            public int compare(String s1, String s2) {
+        List<String> store = new ArrayList<>(words);
 
-                // 정수형으로 비교, compare은 기본적으로 정수형으로 비교해줘야된다.
-                // 사전순으로 출력 (아스키 값)
-                if(s1.length() == s2.length()) {
-                    return s1.compareTo(s2);
-                }
-                else {
-                    // 단어 길이순으로 정렬
-                    return Integer.compare(s1.length(), s2.length());
-                }
+
+        Collections.sort(store, (o1, o2) -> {
+            if(o1.length() == o2.length()){
+                return o1.compareTo(o2); // 길이가 같으면 사전순 정렬
+            }else{
+                return Integer.compare(o1.length(), o2.length());
             }
         });
 
-
-        // 길이 비교가 끝난 배열 첫번째를 출력
-        System.out.println(arr[0]);
-
-        // 앞에있는 배열의 값이랑 비교 (중복 되지 않는 단어만 출력)
-        for(int i = 1; i < size; i++) {
-            if(!(arr[i].equals(arr[i-1]))) {
-                System.out.println(arr[i]);
-            }
+        for(String result : store){
+            System.out.println(result);
         }
-
-
     }
 }
