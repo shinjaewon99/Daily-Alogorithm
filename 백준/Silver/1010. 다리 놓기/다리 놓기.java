99ef16f88000d047d8f1880efb48e59443519dd8
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-09-10 11:48:24 +0900

    [Silver V] Title: 다리 놓기, Time: 220 ms, Memory: 18944 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,36 +1,39 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    private static final int[][] dp = new int[30][30];
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        StringBuilder output = new StringBuilder();
-        // 1. 테스트 갯수 입력
-        int testSize = in.nextInt();
 
-        // 2. dp 배열 요구사항에 맞게 크기 지정
-        long[][] dp = new long[31][31];
+        int test = in.nextInt();
 
 
-        // 3. dp 배열의 값 할당
-        for (int i = 0; i < 31; i++) {
+        for(int i = 0; i < test; i++){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            
+            int result = build(n , m);
+
+            System.out.println(result);
+        }
+    }
+
+    private static int build(int n, int m){
+
+        // 조합에서 nC0, nCn = 1
+        for(int i = 0; i < 30; i++){
             dp[i][0] = 1;
             dp[i][i] = 1;
         }
 
-
-        // 4. 조합 식 구현, 2C1 = 2이다.
-        for (int i = 2; i < 31; i++) {
-            for (int j = 1; j < 31; j++) {
+        // 2C1 은 2
+        for(int i = 2; i < 30; i++){
+            for(int j = 1; j < 30; j++){
                 dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
             }
         }
 
-        // 5. 서쪽 동쪽 입력, 입력된 정수의 해당하는 dp 배열 할당
-        for (int i = 0; i < testSize; i++) {
-            int west = in.nextInt();
-            int east = in.nextInt();
-            output.append(dp[east][west]).append("\n");
-        }
-        System.out.println(output.toString());
+        return dp[m][n];
     }
 }
