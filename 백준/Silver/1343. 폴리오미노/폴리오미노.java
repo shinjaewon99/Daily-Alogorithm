3c1958becaeedb025fc30cced7437ea344d721a1
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-09-23 13:38:59 +0900

    [Silver V] Title: 폴리오미노, Time: 164 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -1,28 +1,22 @@
-import java.util.Scanner;
-
-public class Main {
+import java.util.*;
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-
-        // 1. 입력을 받는다.
-        // 2. AAAA를 기준으로 replace
-        // 3. X 가 남게되는경우 -1 출력
-
         String a = "AAAA";
         String b = "BB";
+        String word = in.nextLine();
 
         String result = "";
-        String input = in.next();
 
-
-        result = input.replace("XXXX", a);
+        // 1. 문자열 대체
+        result = word.replace("XXXX", a);
         result = result.replace("XX", b);
 
 
-        if (result.contains("X")) {
-            System.out.println(-1);
-        } else {
+        // 2. 대체한 문자열에 X가 포함되어 있는 경우
+        if(result.contains("X")){
+            System.out.println("-1");
+        }else{
             System.out.println(result);
         }
     }
