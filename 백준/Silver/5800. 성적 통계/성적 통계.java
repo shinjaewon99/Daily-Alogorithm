e79e451c3b396ba1a0d2a9f615c3fa847d15220d
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-09-26 13:48:34 +0900

    [Silver V] Title: 성적 통계, Time: 248 ms, Memory: 21648 KB -BaekjoonHub

--- 백준/Silver/5800. 성적 통계/성적 통계.java
+++ 백준/Silver/5800. 성적 통계/성적 통계.java
@@ -1,36 +1,31 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-
+import java.util.*;
+class Main {
     public static void main(String[] args) {
-
-
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt(); // 반의 수
 
-
-        for (int i = 0; i < t; i++) {
-            int n = in.nextInt(); // 학생수
-
-            int[] arr = new int[n];
-            int gap = 0;
-
-            for (int j = 0; j < n; j++) {
-                arr[j] = in.nextInt();
+        int test = in.nextInt();
+       
+        
+        for(int i = 0; i < test; i++){
+            int num = in.nextInt();
+            int[] store = new int[num];
+            int largestGap = 0;
+            
+            for(int j = 0; j < num; j++){
+                store[j] = in.nextInt();
             }
 
-            Arrays.sort(arr);
+            Arrays.sort(store);
 
-            // 30 25 76 23 78
-            // 23 25 30 76 78
-            for (int k = 0; k < n - 1; k++) {
-                gap = Math.max(gap, arr[k + 1] - arr[k]); // 값의 차이중 최대값
+            for(int j = 0; j < num - 1; j++){
+                largestGap = Math.max(largestGap, store[j + 1] - store[j]); // 값의 차이중 최대값
             }
 
+            
             System.out.println("Class " + (i + 1));
-            System.out.println("Max " + arr[arr.length - 1] + ", " + "Min " + arr[0] + ", " + "Largest gap " + gap);
-
+            System.out.println("Max " + store[store.length - 1] + ", " + "Min " + store[0] + ", " + "Largest gap " + largestGap);
+            
         }
+       
     }
 }
