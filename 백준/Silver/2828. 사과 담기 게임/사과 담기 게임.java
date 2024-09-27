5d7234d064a382b200d448310c92451baa288c4f
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-09-27 11:25:58 +0900

    [Silver V] Title: 사과 담기 게임, Time: 164 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -1,40 +1,38 @@
-import java.util.Scanner;
-
-public class Main {
-
+import java.util.*;
+class Main {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt(); // 스크린 크기
+
+        int n = in.nextInt();
         int m = in.nextInt(); // 바구니 크기
         int apple = in.nextInt(); // 사과 갯수
 
-        int left = 1; // 바구니의 왼쪽 좌표
-        int right = m; // 바구니의 오른쪽 좌표 (바구니의 크기 끝의 좌표가 오른쪽 좌표)
-        int total = 0;
-
+        int left = 1;
+        int right = m; // 끝 좌표가 바구니 크기
+        int result = 0;
 
         for(int i = 0; i < apple; i++){
-            int temp = in.nextInt();
+            int compare = in.nextInt();
 
             // 바구니 안쪽에 떨어진 경우
-            if(left <= temp && temp <= right){
+            if(left <= compare &&  compare <= right){
                 continue;
             }
-            // 바구니 왼쪽에 가깝게 떨어지는 경우
-            if(left > temp){
-                total +=(left - temp);
-                right -= (left - temp);
-                left = temp;
 
+            // 바구니 왼쪽에 떨어진경우
+            if(left > compare){
+                result += left - compare;
+                right -= left - compare;
+                left = compare;
             }
-            // 바구니 오른쪽에 가깝게 떨어지는 경우
-            else{
-                total += (temp - right);
-                left += (temp - right); // 왼쪽 좌표 갱신
-                right = temp;
+             // 바구니 오른쪽에 떨어진 경우
+             else{
+                result += compare - right;
+                left += compare - right;
+                right = compare; // 오른쪽 좌표 갱신
+                
             }
         }
-        System.out.print(total);
+        System.out.println(result);
     }
 }
