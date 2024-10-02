90b5c7dabfc9c3d2c71a68a13aa99774c158fd32
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-10-02 13:22:03 +0900

    [Silver V] Title: 기상캐스터, Time: 192 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -1,41 +1,40 @@
-import java.util.Scanner;
-
-public class Main {
+import java.util.*;
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
         int h = in.nextInt();
         int w = in.nextInt();
-        char[][] arr = new char[h][w];
-        StringBuilder output = new StringBuilder();
 
-        // 1. 행과열의 길이만큼 arr배열에 할당
-        for (int i = 0; i < h; i++) {
-            String input = in.next();
-            for (int j = 0; j < w; j++) {
-                arr[i][j] = input.charAt(j);
+        char[][] store = new char[h][w];
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < h; i++){
+            String word = in.next();
+            for(int j = 0; j < w; j++){
+                store[i][j] = word.charAt(j);
             }
         }
 
-        // 2. arr 배열 탐색
-        for (int i = 0; i < h; i++) {
+        for(int i = 0; i < h; i++){
             int count = 0;
-            for (int j = 0; j < w; j++) {
-                // 3. 구름이 있는경우
-                if (arr[i][j] == 'c') {
+
+            // 구름이 있는 경우
+            for(int j = 0; j < w; j++){
+                if(store[i][j] == 'c'){
                     count = 1;
-                    output.append("0 ");
-                }else if (arr[i][j] == '.'){
-                    if(count == 0){
-                        output.append("-1 ");
-                    }else{
-                        output.append(count).append(" ");
+                    sb.append("0 ");
+                }else if(store[i][j] == '.'){
+                    if(count != 0){
+                        sb.append(count).append(" ");
                         count++;
+                    }else{
+                        sb.append("-1 ");
                     }
                 }
             }
-            output.append("\n");
+            sb.append("\n");
         }
-        System.out.println(output.toString());
+
+        System.out.println(sb.toString());
     }
 }
