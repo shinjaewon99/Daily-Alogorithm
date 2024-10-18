7582ec42f4fad3f5ae20573124c010f7668e30c7
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-10-18 11:06:04 +0900

    [Silver IV] Title: GCD 합, Time: 176 ms, Memory: 17476 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.java
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -1,36 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // 1. 테스트 갯수
-        int testSize = in.nextInt();
+        int number = in.nextInt();
 
-        // 2. 테스트 수의 갯수
-        for (int i = 0; i < testSize; i++) {
-            // 3. 각 테스트 케이스의 수의 갯수
-            int eachNumbers = in.nextInt();
-            int[] numbers = new int[eachNumbers];
-            for (int j = 0; j < eachNumbers; j++) {
-                numbers[j] = in.nextInt();
-            }
+        for(int i = 0; i < number; i++){
+            int eachSize = in.nextInt();
+            int[] eachNums = new int[eachSize];
 
             long sum = 0;
 
-            // 4. 최대공약수 누적 합
-            for (int j = 0; j < eachNumbers - 1; j++) {
-                for (int k = j + 1; k < eachNumbers; k++) {
-                    sum += gcd(numbers[j], numbers[k]);
+            for(int j = 0; j < eachSize; j++){
+                eachNums[j] = in.nextInt();
+            }
+
+            // 각각의 GCD 구하기
+            for(int k = 0; k < eachSize; k++){
+                for(int p = k + 1; p < eachSize; p++){
+                    sum += gcd(eachNums[k], eachNums[p]);
                 }
             }
+
             System.out.println(sum);
         }
+        
+        
     }
 
-    private static int gcd(int x, int y) {
-        if (y == 0) return x;
+    private static int gcd(final int a, final int b){
+        if(b == 0) return a;
 
-        return gcd(y, x % y);
+        return gcd(b, a % b);
     }
 }
