5492b66370a7d433cdb4f894f25a2d301d729304
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-10-18 10:29:53 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 196 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,74 +1,75 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-
-    static int[] lightCondition;
-    static int lightSize;
-
+class Main {
+    static int num;
+    static int[] numbers;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        lightSize = in.nextInt(); // 스위치의 갯수
-        lightCondition = new int[lightSize + 1]; // 스위치의 상태
-
-        for (int i = 1; i <= lightSize; i++) {
-            lightCondition[i] = in.nextInt();
+        num = in.nextInt();
+        numbers = new int[num + 1];
+        
+        for(int i = 1; i <= num; i++){
+            numbers[i] = in.nextInt();
         }
 
-        int number = in.nextInt(); // 학생수
-        for (int i = 0; i < number; i++) {
-            int student = in.nextInt(); // 학생 성별
-            int studentNum = in.nextInt(); // 학생이 받은 수
-            changeMan(student, studentNum);
-            changeGirl(student, studentNum);
+        int student = in.nextInt();
+        for(int i = 0; i < student; i++){
+            int divStudent = in.nextInt();
+            int compare = in.nextInt();
+            changeMan(divStudent, compare);
+            changeGirl(divStudent, compare);
         }
 
-        // 0 1 1 1 0 1 0 1
-        for (int i = 1; i <= lightSize; i++) {
-            System.out.print(lightCondition[i] + " ");
+        for(int i = 1; i <= num; i++){
+            System.out.print(numbers[i] + " ");
+
+            // 20개씩 출력하기 위함
             if(i % 20 == 0){
-                System.out.println(); // 20개씩 출력하기위해 줄바꿈
+                System.out.println();
             }
         }
+        
     }
 
-    // 남학생일 경우
-    private static void changeMan(int student, int studentNum) {
-        if (student == 1) {
-            for (int i = studentNum; i <= lightSize; i += studentNum) {
-                if (lightCondition[i] == 1) {
-                    lightCondition[i] = 0;
-                } else {
-                    lightCondition[i] = 1;
+    // 남학생인 경우
+    private static void changeMan(final int student, final int divNum){
+        if(student == 1){
+            for(int i = divNum; i <= num; i+= divNum){
+                if(numbers[i] == 1){
+                    numbers[i] = 0;
+                }else{
+                    numbers[i] = 1;
                 }
             }
         }
     }
-
-    // 여학생일 경우
-    private static void changeGirl(int student, int studentNum) {
-        if (student == 2) {
-            // 이진 탐색을 위한 left, right
-            int left = studentNum - 1;
-            int right = studentNum + 1;
-
-            while (left >= 1 && right <= lightSize) {
-                // 대칭인 스위치가 다른경우
-                if (lightCondition[left] != lightCondition[right]) {
+    
+    // 여학생인 경우
+    private static void changeGirl(final int student, final int divNum){
+        if(student == 2){
+            // 이진 탐색
+            int left = divNum - 1;
+            int right = divNum + 1;
+    
+            // 어디까지 같은지 확인
+            while(left >= 1 && right <= num){
+    
+                if(numbers[left] != numbers[right]){
                     break;
                 }
+                
                 left--;
                 right++;
             }
-
-            // right 까지인 이유는 while문을 돌며 검증하는 과정에서 right 값이 1 더 증가하게됩니다.
-            for (int i = left + 1; i < right; i++) {
-                if (lightCondition[i] == 1) {
-                    lightCondition[i] = 0;
-                } else {
-                    lightCondition[i] = 1;
+    
+            // while문에서 left와 right의 값이 감소 혹은 증가한 상태로 탈출 하기 때문
+            for(int i = left + 1; i < right; i++){
+                 if(numbers[i] == 1){
+                    numbers[i] = 0;
+                }else{
+                    numbers[i] = 1;
                 }
             }
         }
     }
-
 }
