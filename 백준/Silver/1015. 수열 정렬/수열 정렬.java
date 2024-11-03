b6eefc282025941beb765f1b3d9a1dad23923ba1
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-11-03 20:20:45 +0900

    [Silver IV] Title: 수열 정렬, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

--- 백준/Silver/1015. 수열 정렬/수열 정렬.java
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -1,31 +1,29 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // 1. 크기입력, 배열 크기지정
+        int inputSize = in.nextInt();
+        int[] store1 = new int[inputSize];
+        int[] store2 = new int[inputSize];
         StringBuilder output = new StringBuilder();
-        int size = in.nextInt();
-        int[] arr1 = new int[size];
-        int[] arr2 = new int[size];
 
-        // 2. 배열의 값 입력
-        for (int i = 0; i < size; i++) {
-            arr1[i] = in.nextInt();
-            arr2[i] = arr1[i];
+        for(int i = 0; i < inputSize; i++){
+            store1[i] = in.nextInt();
+            store2[i] = store1[i]; 
         }
 
-        // 3. 오름차순 정렬
-        Arrays.sort(arr2);
+        Arrays.sort(store2);
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (arr1[i] == arr2[j]) {
+        // 2 1 3 1
+        // 1 1 2 3
+        for(int i = 0; i < inputSize; i++){
+            for(int j = 0; j < inputSize; j++){
+                if(store1[i] == store2[j]){
                     output.append(j).append(" ");
-                    // 4. 배열에서 값을 찾았으면 -1으로 할당, 추가적인 탐색을 막기위해
-                    arr2[j] = -1;
+                    // 중복 체크를 방지
+                    store2[j] = -1;
                     break;
                 }
             }
