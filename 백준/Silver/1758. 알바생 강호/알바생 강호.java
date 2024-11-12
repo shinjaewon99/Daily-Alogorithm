4065051740569c65fd0f977ab148d7aa5e79c267
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-11-12 13:22:58 +0900

    [Silver IV] Title: 알바생 강호, Time: 816 ms, Memory: 105240 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.java
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -1,35 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // 1. 사람의 수 입력
-        int num = in.nextInt();
-
-        // 2. 각 사람이 주려고 하는 팁
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < num; i++) {
-            list.add(in.nextInt());
-        }
-
-        // 3. 강호가 받을수 있는 팁 최대값 구하기
-        list.sort(Collections.reverseOrder());
+        int t = in.nextInt();
         long result = 0;
-        int index = 0;
-        for (int i = 1; i <= num; i++) {
-            int temp = list.get(index) - (i - 1);
 
-            if (temp <= 0) {
-                temp = 0;
-            }
-            result += temp;
-            index++;
+        List<Integer> store = new ArrayList<>();
+        
+        for(int i = 0; i < t; i++){
+            store.add(in.nextInt());
         }
-        System.out.println(result);
+        Collections.sort(store, Collections.reverseOrder());
 
+        for(int i = 0; i < t; i++){
+             // 팁 계산
+            int calcu = store.get(i) - i;
+
+            if(calcu > 0){
+                result += calcu;
+            }
+        }
+
+        System.out.println(result);
     }
 }
