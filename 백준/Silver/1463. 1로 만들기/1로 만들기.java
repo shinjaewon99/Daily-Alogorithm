9194d64d258ba90c2b66e7f6b4bd190a5158e02f
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-11-25 13:54:58 +0900

    [Silver III] Title: 1로 만들기, Time: 192 ms, Memory: 21592 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,34 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // 1. 정수 N 입력
         int number = in.nextInt();
-        int[] dp = new int[number + 1];
+        int[] store = new int[number + 1];
 
-        // 2. dp 배열 값 초기화 
-        dp[0] = 0;
-        dp[1] = 0;
+        for(int i = 2; i <= number; i++){
+            store[i] = store[i - 1] + 1;
 
-        // dp[3] = 1
-        // dp[4] = 2
-        // dp[5] = 3
-        //.. dp[9] = 2;
-        for (int i = 2; i <= number; i++) {
-            dp[i] = dp[i - 1] + 1;
-
-            // 2로 나눠질 경우
+            // 2로 나누어 떨어지면
             if(i % 2 == 0){
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+                store[i] = Math.min(store[i], store[i / 2] + 1);
             }
 
-            // 3으로 나눠질 경우
+            // 3로 나누어 떨어지면
             if(i % 3 == 0){
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+                store[i] = Math.min(store[i], store[i / 3] + 1);
             }
         }
-        System.out.println(dp[number]);
+
+    
+        System.out.println(store[number]);
     }
 }
