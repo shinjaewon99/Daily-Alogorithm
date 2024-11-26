ee2cd2b4fece54aaf3806e50642f5c44a248df9b
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-11-26 10:57:06 +0900

    [Silver III] Title: 피보나치 함수, Time: 204 ms, Memory: 18496 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,27 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int size = in.nextInt();
+        int t = in.nextInt();
 
-        while (size-- > 0) {
-            int number = in.nextInt();
-            int[] fibo0 = new int[41]; // number의 최대 
-            int[] fibo1 = new int[41];
-            fibo0[0] = 1;
-            fibo0[1] = 0;
-            fibo1[0] = 0;
-            fibo1[1] = 1;
+        int[][] dp = new int[41][2];
 
-            // dp 탐색
-            for (int i = 2; i <= number; i++) {
-                fibo0[i] = fibo0[i - 2] + fibo0[i - 1];
-                fibo1[i] = fibo1[i - 2] + fibo1[i - 1];
-            }
 
-            System.out.println(fibo0[number] + " " + fibo1[number]);
+        // 초기값 설정
+        dp[0][0] = 1; // fibo(0)의 0호출 횟수
+        dp[0][1] = 0; // fibo(0)의 1호출 횟수
+        dp[1][0] = 0; // fibo(1)의 0호출 횟수
+        dp[1][1] = 1; // fibo(1)의 1호출 횟수
+
+        for(int i = 2; i < 41; i++){
+            dp[i][0] = dp[i - 2][0] + dp[i - 1][0];
+            dp[i][1] = dp[i - 2][1] + dp[i - 1][1];
         }
+        
+        for(int i = 0; i < t; i++){
+            int number = in.nextInt();
+
+            System.out.println(dp[number][0] + " " + dp[number][1]);
+        }
+        
     }
 }
