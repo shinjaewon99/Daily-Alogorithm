9da8f717896f40eebfba70d3316919bf3e4365a9
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-12-05 10:23:29 +0900

    [Silver III] Title: Four Squares, Time: 200 ms, Memory: 17952 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,24 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // 1. 자연수 n 입력
         int number = in.nextInt();
         int[] dp = new int[number + 1];
-
-        // 1 = 1^2
-        // 2 = 1^2 + 1^2
         dp[1] = 1;
 
-        // 25 = 4^2 + 3^2 가 아닌 5^2 최소를 선택
-        for (int i = 2; i <= number; i++) {
-            dp[i] = dp[i - 1] + 1;
+        for(int i = 2; i <= number; i++){
+            dp[i] = i; // 최대값은 1^2개로 i개 사용
+
             for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // dp 재할당
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
+        
 
         System.out.println(dp[number]);
     }
