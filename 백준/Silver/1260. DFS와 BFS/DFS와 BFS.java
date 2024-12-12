34ddbaca84a9fbfabba2439cf722c88bb88821c0
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-12-12 10:59:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 496 ms, Memory: 42700 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,31 +1,27 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    private static int number;
-    private static int connect;
+class Main {
+    private static int n; // 정점의 갯수
+    private static int m; // 간선의 갯수
     private static int start;
     private static int[][] graph;
-    private static boolean[] flag;
+    private static boolean[] visit;
+    
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-
-        // 1. 정점의 갯수 입력
-        number = in.nextInt();
-
-        // 2. 간선의 갯수 입력
-        connect = in.nextInt();
-
-        // 3. 탐색을 시작할 번호
+        n = in.nextInt();
+        m = in.nextInt();
         start = in.nextInt();
 
-        // 4. graph 배열 초기화
-        graph = new int[number + 1][number + 1];
-        flag = new boolean[number + 1];
-        for (int i = 1; i <= connect; i++) {
+        graph = new int [n + 1][n + 1];
+        visit = new boolean[n + 1];
+
+        for(int i = 0; i < m; i++){
             int x = in.nextInt();
             int y = in.nextInt();
+
+            // 양방향으로 표시
             graph[x][y] = graph[y][x] = 1;
         }
 
@@ -33,36 +29,36 @@ public class Main {
         System.out.println();
 
         // 방문 배열 초기화
-        flag = new boolean[number + 1];
+        visit = new boolean[n + 1];
         bfs(start);
     }
-    // 5. dfs 깊이 우선 탐색
-    private static void dfs(int start){
-        flag[start] = true;
 
-        System.out.print(start + " ");
-        for (int i = 1; i <= number; i++) {
-            if(!flag[i] && graph[start][i] == 1){
+    private static void dfs(int depth){
+        visit[depth] = true; // 방문 처리
+        System.out.print(depth + " ");
+
+        for(int i = 1; i <= n; i++){
+            if(!visit[i] && graph[depth][i] == 1){
                 dfs(i);
             }
         }
     }
 
-    // 6. bfs 넢이 우선 탐색
-    private static void bfs(int start){
+    private static void bfs(int depth) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        flag[start] = true;
-
+        queue.add(depth);
+        visit[depth] = true;
+        
         while(!queue.isEmpty()){
-            int poll = queue.poll();
-            System.out.print(poll + " ");
 
-            // 시작한 번호에서 끝까지 탐색한다.
-            for (int i = 1; i <= number; i++) {
-                if(!flag[i] && graph[poll][i] == 1){
+            int number = queue.poll();
+            System.out.print(number + " ");
+
+            // 너비로 탐색
+            for(int i = 1; i <= n; i++){
+                if(!visit[i] && graph[number][i] == 1){
                     queue.add(i);
-                    flag[i] = true;
+                    visit[i] = true;
                 }
             }
         }
