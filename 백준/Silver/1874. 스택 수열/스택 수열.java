5df64663b3b7297e48362a2d99098ba1553089f4
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-12-12 13:57:40 +0900

    [Silver II] Title: 스택 수열, Time: 836 ms, Memory: 107220 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,33 +1,39 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
 
+        Stack <Integer> store = new Stack<>();
+        StringBuilder result = new StringBuilder();
         int size = in.nextInt();
-        int start = 0;
-        for (int i = 0; i < size; i++) {
 
-            int value = in.nextInt();
+        // 시작 숫자
+        int start = 1;
+        
+         for (int i = 0; i < size; i++) {
+            int putNumber = in.nextInt();
 
-            if (value > start) {
-                for (int j = start + 1; j <= value; j++) {
-                    stack.push(j); // 스택에 start + 1 부터 값 저장
-                    sb.append("+").append("\n");
-                }
-                start = value; // start를 value값으로 초기화
-            } else if (stack.peek() != value) {
+            // 필요한 수까지 스택에 push
+            while (start <= putNumber) {
+                store.push(start);
+                result.append("+").append("\n");
+                start++;
+            }
+
+            // 스택에서 꺼낼 수 있는지 확인
+            if (store.peek() == putNumber) {
+                store.pop();
+                result.append("-").append("\n");
+            } else {
+                // 수열을 만들 수 없는 경우
                 System.out.println("NO");
                 return;
             }
-            
-            stack.pop();
-            sb.append("-").append("\n");
         }
-        System.out.println(sb.toString());
+
+        System.out.println(result.toString());
+        
     }
 }
