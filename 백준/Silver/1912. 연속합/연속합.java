63a9d18a30280c7ebd4a55b5602415a2010b77fc
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2024-12-13 10:25:46 +0900

    [Silver II] Title: 연속합, Time: 700 ms, Memory: 108728 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,28 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // 1. 정수의 갯수 입력
-        int size = in.nextInt();
-        int[] numbers = new int[size + 1];
-        int[] dp = new int[size + 1];
-
-        // 2. 정수의 갯수만큼 숫자 입력
-        for (int i = 1; i <= size; i++) {
-            numbers[i] = in.nextInt();
+        int n = in.nextInt();
+        int[] store = new int[n + 1];
+        int[] dp = new int[n + 1];
+        
+        for(int i = 1; i <= n; i++){
+            store[i] = in.nextInt();
         }
 
-        // 3. dp 배열 값 할당
-        dp[1] = numbers[1];
-        int max = numbers[1];
+        dp[1] = store[1];
+        int max = store[1];
 
-        // 4. 연속된 최대값 찾기
-        // 입력값 : 2 1 -4 3 4 -4 6 5 -5 1
-        // dp 배열 : 2 3 -1 2 6 2 8 13 8 9
-        for (int i = 2; i <= size; i++) {
-            dp[i] = Math.max(dp[i - 1] + numbers[i], numbers[i]);
+        for(int i = 2; i <= n; i++){
+            // 연속된 합 vs 현재 인덱스값
+            dp[i] = Math.max(dp[i - 1] + store[i], store[i]);
             max = Math.max(max, dp[i]);
         }
 
