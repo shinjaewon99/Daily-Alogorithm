fc8fc8d71b19bb20726fdd0f5cf65a702c91484b
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2025-08-01 16:45:43 +0900

    [Bronze IV] Title: 베라의 패션, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+
+        System.out.println(n * (n-1));
+    }
+}
