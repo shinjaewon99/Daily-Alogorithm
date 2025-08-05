3da3cb54cced19b8060abca30f33fc129c6ebfe2
신재원 <95893341+shinjaewon99@users.noreply.github.com>
2025-08-05 16:43:56 +0900

    [Gold IV] Title: 줄세우기, Time: 228 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1); 
+
+        int result = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        // 옮겨야 하는 최소 인원
+        System.out.println(n - result);
+    }
+}
